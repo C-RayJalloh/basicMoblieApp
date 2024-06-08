05d15021882a6e631732ba4dee82cf8304cfe5b6
“Jahrulo” <mserayjay@gmail.com>
2024-06-08 13:20:19 +0000

    initial commit

---
+++ components/ElevatedCard.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function ElevatedCard() {
+  return (
+    <View>
+      <Text style={styles.heading}>ElevatedCard</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    
+});
